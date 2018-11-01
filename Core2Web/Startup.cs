219fb7e3a441765fe569ed11f0e2dc7f33f40d7a
Victor Khabarov <victor.khabarov@custdata.com>
2018-11-01 12:40:12 -0400

    Add PROGECT

---
+++ Core2Web/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DevExpress.AspNetCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using DevExpress.AspNetCore.Reporting;
+
+
+namespace Core2Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDevExpressControls(options =>
+            {
+
+            });
+            services.ConfigureReportingServices(configurator =>
+            {
+                configurator.ConfigureReportDesigner(designerConfigurator =>
+                {
+                    designerConfigurator.RegisterDataSourceWizardConfigFileConnectionStringsProvider();
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            DevExpress.XtraReports.Configuration.Settings.Default.UserDesignerOptions.DataBindingMode = DevExpress.XtraReports.UI.DataBindingMode.Expressions;
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseDevExpressControls();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ Core2Web/wwwroot/js/site.js
@@ -0,0 +1,80 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+/*
+ * checks and sends a the given devExtreme form to the given url.
+ *
+ * returns a promise 
+ */
+function SubmitDxForm(dxFormId, keyField , url) {
+    var form = $('#' + dxFormId).dxForm('instance');
+    if (form.validate().isValid) {
+        var data = form.option("formData");
+        var send;
+        if (!data[keyField] || data[keyField] == 0) {
+            send = JSON.stringify(data);
+        } else {
+            send = JSON.stringify({ key: data[keyField], values: data });
+        }
+        return fetch(url,
+            {
+                method: 'POST',
+                body: send,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'dataType': 'json',
+                    'charset':'utf-8'
+                }
+            }).then(checkRes);
+    }
+    return Promise.reject("form not valid");
+}
+
+function SubmitForm(formId, url) {
+    var form = $("#"+formId)[0];
+    return fetch(url,
+        {
+            method: 'POST',
+            body: new FormData(form)
+        });
+}
+
+function checkRes(res) {
+    return res.status != '200' ? Promise.reject(res) : Promise.resolve(res);
+}
+
+// Comes From ExToasdtrAjax
+function postToast(res) {
+    var msgs = JSON.parse(res.headers.get('X-NToastNotify-Messages'));
+
+    msgs.forEach(function (i) {
+        toastr.options = i.options;
+        toastr[i.options.type](i.message,'',i.options);
+    });
+    return Promise.resolve(res);
+}
+
+function postToastAfterNav() {
+    var msgs = JSON.parse(res.headers.get('X-NToastNotify-Messages'));
+
+    msgs.forEach(function (i) {
+        toastr.options = i.options;
+        toastr[i.options.type](i.message, '', i.options);
+    });
+    return Promise.resolve(res);
+}
+
+function NavToUrl(url) {
+    return function(res) {
+        window.location.href = url;
+        return Promise.resolve(res);
+    }
+}
+
+function reloadPage(res) {
+    window.location.reload();
+    return Promise.resolve(res);
+}
